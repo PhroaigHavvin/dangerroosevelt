cca9dbbf546f0604fc129caedf1890127ec375ee
shabutie325 <brvasque@gmail.com>
2017-12-26 16:31:12 -0600

    Facebook integration, color changes
    Added facebook integration and some color changes to make each section more distinct from each other

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var animating = false;
+
 $(document).ready(function(){
 	$("a").on('click', function(event){
 		if (this.hash !== "") {
@@ -7,9 +9,11 @@ $(document).ready(function(){
 
 			var offsetMenu = (hash != "#home") ? $(hash).offset().top - 140 : 0;
 
+			animating = true;
+
 			$('html,body').animate({
 				scrollTop: offsetMenu
-			}, 1200);
+			}, 1200, function(){animating=false;$(window).scroll()});
 		}
 	});
 });
@@ -19,24 +23,26 @@ $(window).scroll(function() {
 	var musicTop = $('#music').offset().top - 190;
 	var aboutTop = $('#about').offset().top - 400;
 
-	if (scroll < musicTop) {
-		$('#home-link').addClass('selected');
-		$('#music-link').removeClass('selected');
-		$('#about-link').removeClass('selected');
-	} 
-	else if (scroll >= musicTop && scroll < aboutTop) {
-		$('#home-link').removeClass('selected');
-		$('#music-link').addClass('selected');
-		$('#about-link').removeClass('selected');
-	}
-	else if (scroll >= aboutTop) {
-		$('#home-link').removeClass('selected');
-		$('#music-link').removeClass('selected');
-		$('#about-link').addClass('selected');
-	}
-	else {
-		$('#home-link').addClass('selected');
-		$('#music-link').removeClass('selected');
-		$('#about-link').removeClass('selected');
+	if(!animating){
+		if (scroll < musicTop) {
+			$('#home-link').addClass('selected');
+			$('#music-link').removeClass('selected');
+			$('#about-link').removeClass('selected');
+		} 
+		else if (scroll >= musicTop && scroll < aboutTop) {
+			$('#home-link').removeClass('selected');
+			$('#music-link').addClass('selected');
+			$('#about-link').removeClass('selected');
+		}
+		else if (scroll >= aboutTop) {
+			$('#home-link').removeClass('selected');
+			$('#music-link').removeClass('selected');
+			$('#about-link').addClass('selected');
+		}
+		else {
+			$('#home-link').addClass('selected');
+			$('#music-link').removeClass('selected');
+			$('#about-link').removeClass('selected');
+		}
 	}
 });
